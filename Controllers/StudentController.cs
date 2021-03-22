593ea544e24531a92ab8b86117e90f0b1bbd5a71
Admin <Admin@KANINI-LTP-148>
2021-03-22 18:56:04 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,115 @@
+﻿using AspCoreApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreApp.Controllers
+{
+    public class StudentController : Controller
+    {
+         StudentDetails details = new StudentDetails();
+        // GET: StudentController
+        public static List<StudentDetails> students = StudentDetails.GetStudentDetails();
+        public ActionResult Studentinfo()
+        {
+            
+            return View(students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+          
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StudentDetails studentDetails)
+        {
+            try
+            {
+                details.AddDetails(studentDetails);                //student.AddDetails(student);
+
+                return RedirectToAction("Studentinfo");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            StudentDetails data = students.FirstOrDefault(x => x.Sid == id);
+            return View(data);
+           // return View();
+
+            
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, StudentDetails studentDetails)
+        {
+            try
+            {
+                StudentDetails data = students.FirstOrDefault(x => x.Sid == id);
+                /*if (studentDetails != null)
+                {*/
+                data.Sid= studentDetails.Sid;
+                data.SName = studentDetails.SName;
+                data.Address = studentDetails.Address;
+
+                
+                    
+                    return RedirectToAction("Studentinfo");
+                    
+               //}
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            StudentDetails data = students.FirstOrDefault(x => x.Sid == id);
+            details.DeleteDetails(data);
+            return RedirectToAction("Studentinfo");
+
+            //return View();
+        }
+
+        // POST: StudentController/Delete/5
+        /*[HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, StudentDetails studentDetails)
+        {
+            try
+            {
+                StudentDetails data = students.FirstOrDefault(x => x.Sid == id);
+                details.DeleteDetails(studentDetails);
+                return RedirectToAction("Studentinfo");
+            }
+            catch
+            {
+                return View();
+            }
+        }*/
+    }
+}
---
+++ Models/StudentDetails.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreApp.Models
+{
+    public class StudentDetails
+    {
+        public int Sid { get; set; }
+        public string SName { get; set; }
+        public string Address { get; set; }
+
+        public StudentDetails()
+        {
+
+        }
+
+        public StudentDetails(int sid, string sname, string addr)
+        {
+            Sid = sid;
+            SName = sname;
+            Address = addr;
+        }
+
+      public static  List<StudentDetails> students = new List<StudentDetails>();
+        public static List<StudentDetails> GetStudentDetails()
+        {
+           // students.Clear();
+            students.Add(new StudentDetails(101, "Akash", "xxxxx"));
+            students.Add(new StudentDetails(102, "Ajay", "yyyyy"));
+            students.Add(new StudentDetails(103, "Raj", "zzzzzz"));
+            students.Add(new StudentDetails(104, "Hari", "vvvvvv"));
+            students.Add(new StudentDetails(105, "Jack", "tttttt"));
+            return students;
+
+        }
+
+     
+
+        public void AddDetails(StudentDetails student)
+        {
+            students.Add(student);
+        }
+
+        public void DeleteDetails(StudentDetails student)
+        {
+            students.Remove(student);
+        }
+    }
+
+    
+}
+
